22f44b0f1996fc907a6db9f2a6a50c1be63ff8a5
Francis ANDRE <francis.andre.kampbell@orange.fr>
2020-02-23 17:16:56 +0100

    Fix Java1.5 actions

--- grammars/cpp/java_1.5/src/main/cpp/main.cpp
+++ grammars/cpp/java_1.5/src/main/cpp/main.cpp
@@ -1,3 +1,93 @@
+#include <iostream>
+#include <fstream>
+#include <iomanip>
+#include <iostream>
+#include <string>
+#include "JavaParser.h"
+
+#include "JavaCC.h"
+#include "JavaParserTokenManager.h"
+#include "ParseException.h"
+#include "StreamReader.h"
+
+using namespace std;
+
+JJString ReadFileFully() {
+	JJString code;
+#if 0
+	code =
+		"int n;\n"
+		"int fact;\n"
+		"read n;\n"
+		"fact = 1;\n"
+		"while (n > 1)\n"
+		"{\n"
+		"		fact = fact * n;\n"
+		"		n = n - 1;\n"
+		"}\n"
+		"write fact;\n";
+#else
+	code = "int foo;\nfoo = 3 + 4 * 5 + 6;\nwrite foo;\n";
+#endif
+
+	return code;
+}
+static void usage(int argc, const char* argv) {
+}
+int main(int argc, const char* argv) {
+	istream*	input  = &cin;
+	ostream*	output = &cout;
+	ostream*	error  = &cerr;
+	ifstream	spl;
+	ifstream	ifs;
+	ofstream	ofs;
+	ofstream	efs;
+	StreamReader*	sr = nullptr;
+	CharStream *	cs = nullptr;
+
+	try {
+		if (argc == 5) {
+			spl.open(argv[1]);
+			ifs.open(argv[2]);
+			ofs.open(argv[3]);
+			efs.open(argv[4]);
+			if (ifs.is_open() && ofs.is_open() && efs.is_open() && spl.is_open()) {
+				input = &ifs;	output = &ofs;	error = &efs;
+				sr = new StreamReader(spl);
+				cs = new CharStream(sr);
+			}
+			else {
+				cerr << "cannot open spl or in or out or err file" << endl;
+				return 8;
+			}
+		} else
+		if (argc == 2) {
+			JJString s = ReadFileFully();
+			cout << s << endl;
+			cs = new CharStream(s.c_str(), s.size() - 1, 1, 1);
+		}
+		else {
+			usage(argc, argv);
+			return 0;
+		}
+
+		JavaParserTokenManager *scanner = new JavaParserTokenManager(cs);
+		JavaParser parser(scanner);
+		parser.CompilationUnit();
+	} catch (const ParseException& e) {
+		clog << e.expectedTokenSequences << endl;
+	}
+	catch (...) {
+
+	}
+	if (spl.is_open()) spl.close();
+	if (ifs.is_open()) ifs.close();
+	if (ofs.is_open()) ofs.close();
+	if (efs.is_open()) efs.close();
+	if (cs) delete cs;
+	if (sr) delete sr;
+	return 0;
+}
 #if 0
 import java.io.*;
 
@@ -130,90 +220,3 @@ public class JavaParser
 
 }
 #endif
-#include <iostream>
-#include <fstream>
-#include <iomanip>
-#include <iostream>
-#include <string>
-#include <stdlib.h>
-#include "JavaParser.h"
-
-#include "JavaCC.h"
-#include "JavaParserTokenManager.h"
-#include "ParseException.h"
-#include "StreamReader.h"
-using namespace std;
-JJString ReadFileFully() {
-	JJString code;
-#if 0
-	code =
-		"int n;\n"
-		"int fact;\n"
-		"read n;\n"
-		"fact = 1;\n"
-		"while (n > 1)\n"
-		"{\n"
-		"		fact = fact * n;\n"
-		"		n = n - 1;\n"
-		"}\n"
-		"write fact;\n";
-#else
-	code = "int foo;\nfoo = 3 + 4 * 5 + 6;\nwrite foo;\n";
-#endif
-
-	return code;
-}
-int main(int argc, const char* argv) {
-	istream*	input  = &cin;
-	ostream*	output = &cout;
-	ostream*	error  = &cerr;
-	ifstream	spl;
-	ifstream	ifs;
-	ofstream	ofs;
-	ofstream	efs;
-	StreamReader*	sr = nullptr;
-	CharStream *	cs = nullptr;
-
-	try {
-		if (argc == 5) {
-			spl.open(argv[1]);
-			ifs.open(argv[2]);
-			ofs.open(argv[3]);
-			efs.open(argv[4]);
-			if (ifs.is_open() && ofs.is_open() && efs.is_open() && spl.is_open()) {
-				input = &ifs;	output = &ofs;	error = &efs;
-				sr = new StreamReader(spl);
-				cs = new CharStream(sr);
-			}
-			else {
-				cerr << "cannot open spl or in or out or err file" << endl;
-				return 8;
-			}
-		} else
-		if (argc == 2) {
-			JJString s = ReadFileFully();
-			cout << s << endl;
-			cs = new CharStream(s.c_str(), s.size() - 1, 1, 1);
-		}
-		else {
-			usage(argc, argv);
-			return 0;
-		}
-
-		JavaParserTokenManager *scanner = new JavaParserTokenManager(cs);
-		JavaParser parser(scanner);
-		parser.CompilationUnit();
-	} catch (const ParseException& e) {
-		clog << e.expectedTokenSequences << endl;
-	}
-	catch (...) {
-
-	}
-	if (spl.is_open()) spl.close();
-	if (ifs.is_open()) ifs.close();
-	if (ofs.is_open()) ofs.close();
-	if (efs.is_open()) efs.close();
-	if (cs) delete cs;
-	if (sr) delete sr;
-	return 0;
-}
