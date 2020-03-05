5fb6a828b4faeb7257d9df6a5947e78634c10453
Francis ANDRE <francis.andre.kampbell@orange.fr>
2020-03-05 15:04:08 +0100

    Update module

--- grammars/cpp/java_1.1/HelloWorld.java
+++ grammars/cpp/java_1.1/HelloWorld.java
@@ -1,7 +1,7 @@
 package test;
 
-class HelloWorld {
-	public static void main(String args[]) {
-		System.out.println("Hello World!");
-	}
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
 }
