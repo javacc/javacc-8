3de8f8f89baaf6a80a0081ea065c8f1da9c420d8
Francis ANDRE <francis.andre.kampbell@orange.fr>
2020-03-10 22:19:38 +0100

    Move integration-tests to codegen

--- examples/cpp/Interpreter/VST/vs2017spl/Interpreter/AST/pch.cpp
+++
@@ -1,5 +0,0 @@
-// pch.cpp : fichier source correspondant à l'en-tête précompilé ; nécessaire au bon déroulement de la compilation
-
-#include "pch.h"
-
-// Vous pouvez en général ignorer ce fichier, mais gardez-le si vous utilisez des en-têtes précompilés.
