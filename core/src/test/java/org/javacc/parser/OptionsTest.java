82ddaa4a2b07ec5d84981779f3d8673df7630bea
Francis ANDRE <francis.andre.kampbell@orange.fr>
2020-03-18 12:53:15 +0100

    New/changed c++ options

--- core/src/test/java/org/javacc/parser/OptionsTest.java
+++ core/src/test/java/org/javacc/parser/OptionsTest.java
@@ -16,7 +16,7 @@ public final class OptionsTest extends TestCase {
     Options.init();
     Context context = new Context();
 
-    assertEquals(52, Options.optionValues.size());
+    assertEquals(53, Options.optionValues.size());
 
     assertEquals(true, Options.getBuildParser());
     assertEquals(true, Options.getBuildTokenManager());
